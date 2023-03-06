c84e0791e5b229daab42d09280442328aa398c8a
Aslam <sayedaslam593@gmail.com>
2023-03-06 14:40:16 +0530

    initial

---
+++ test.1/city.js
@@ -0,0 +1,5 @@
+const city1 = 'Kerala'
+const city2 = 'Bangalore'
+const city3 = 'Chennai'
+
+module.exports = {city1,city2,city3}
---
+++ test.1/cityFunction.js
@@ -0,0 +1,7 @@
+const CityFunction = (city)=>{
+
+
+    console.log(`my city is ${city}`)
+}
+
+module.exports = CityFunction
